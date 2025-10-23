aac6e699556533a6bba35d21f33e224c1d73efc4
PehansaPiyamini1 <as20240469@sci.sjp.ac.lk>
2025-10-23 19:47:10 +0530

    city function call in main

--- Logistic Management System/main.c
+++ Logistic Management System/main.c
@@ -43,6 +43,20 @@ void cityManagement(char city [NUMBER_OF_CITIES]){
 int main()
 {
 
+    char Colombo='A',Kandy='B',Galle='C',Jaffna='D',Anuradhapura='E',Polonnaruwa='f',Kurunagala='G',Rathnapura='H',
+    Trincomalee='I',Batticaloa='J',Matara='K',Badulla='L',NuwaraEliya='M',Hambanthota='N',Kaluthara='O',Negambo='P',
+    Puttalam='Q',Chilaw='R',Vavuniya='S',Ampara='T',Monaragala='U',Kegalle='V',Gampaha='W',Dehiwala='X',
+    Moratuwa='Y',Maharagama='Z',Wattala='a',Beruwala='b',Panadura='d',Katunayaka='e';
+
+    printf("1.City Management\n\n");
+    printf("Colombo=A        Kandy=B        Galle=C         Jaffna=D         Anuradhapura=E\nPolonnaruwa=F    Kurunagala=G   Rathnapura=H    Trincomalee=I    Batticaloa=J\nMatara=K         Badulla=L      NuwaraEliya=M   Hambanthota=N    Kaluthara=O\nNegambo=P        Puttalam=Q     Chilaw=R        Vavuniya=S       Ampara=T\nMonaragala=U     Kegalle=V      Gampaha=W       Dehiwala=X       Moratuwa=Y\n");
+    printf("Maharagama=Z     Wattala=a      Beruwala=b      Panadura=d       Katunayaka=e\n\n");
+
+
+    char city[NUMBER_OF_CITIES]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','d','e'};
+
+    cityManagement(city);
+
 
 
 
