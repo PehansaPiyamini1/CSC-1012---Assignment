b3ae692faae0d9d66220985c47f66e960fa5478d
PehansaPiyamini1 <as20240469@sci.sjp.ac.lk>
2025-10-23 19:43:58 +0530

    create cityManagement function to city management

--- Logistic Management System/main.c
+++ Logistic Management System/main.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NUMBER_OF_CITIES 30
+
+
+int a;
+char city [NUMBER_OF_CITIES];
+
+
+void cityManagement(char city [NUMBER_OF_CITIES]);
+
+
+void cityManagement(char city [NUMBER_OF_CITIES]){
+
+    for(a=0;a<NUMBER_OF_CITIES;a++){
+        printf("%c    ",city[a]);
+    }
+    printf("\n\n");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
-    printf("Hello world!\n");
+
+
+
+
+
+
+
     return 0;
 }
